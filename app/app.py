6d0302409a13287035690985cce4f72be0529a07
silverfatt <silvestr.fat@mail.ru>
2022-11-04 16:29:40 +0300

    added simple es operations

--- app/app.py
+++ app/app.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 
-from fastapi import FastAPI
+from elasticsearch import Elasticsearch
+from fastapi import FastAPI, HTTPException, status
 from sqlalchemy.orm import sessionmaker
 
 from database import prepare_db, engine, Document, DB_URL
 from schemas import DocumentScheme
 
 app = FastAPI()
+es = Elasticsearch(hosts=['http://localhost:9200'])
 try:
     prepare_db()
 except Exception:
@@ -30,7 +32,16 @@ async def add_document(document: DocumentScheme):
     """
     new_document = Document(rubrics=document.rubrics, text=document.text, created_date=datetime.now())
     s.add(new_document)
-    s.commit()
+    new_doc_to_es = {
+        'text': new_document.text
+    }
+    try:
+        resp = es.index(index="doc-index", id=new_document.doc_id, document=new_doc_to_es)
+        s.commit()
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_502_BAD_GATEWAY,
+                            detail="ElasticSearch server is not available. Did not add anything")
+    print(resp)
     return {'rubrics': new_document.rubrics, 'text': new_document.text, 'created_date': new_document.created_date}
 
 
@@ -43,5 +54,11 @@ async def delete_document(doc_id: int):
     if not document_to_delete:
         return {'not exists': doc_id}
     s.delete(document_to_delete)
-    s.commit()
+    try:
+        resp = es.delete(index="doc-index", id=doc_id)
+        s.commit()
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_502_BAD_GATEWAY,
+                            detail="ElasticSearch server is not available. Did not delete anything")
+    print(resp)
     return {'deleted': doc_id}
