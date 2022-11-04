df1aa37dabde27260f01dd9e476691518f193555
silverfatt <silvestr.fat@mail.ru>
2022-11-04 13:10:03 +0300

    document delete method

--- app/app.py
+++ app/app.py
@@ -22,3 +22,13 @@ async def add_document(document: DocumentScheme):
     s.add(new_document)
     s.commit()
     return {'rubrics': new_document.rubrics, 'text': new_document.text, 'created_date': new_document.created_date}
+
+
+@app.delete("/delete")
+async def delete_document(doc_id: int):
+    document_to_delete = s.query(Document).filter(Document.doc_id == doc_id).first()
+    if not document_to_delete:
+        return {'not exists': doc_id}
+    s.delete(document_to_delete)
+    s.commit()
+    return {'deleted': doc_id}
