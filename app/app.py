97d471f0eeee716b653c67afdb879997bcd46c76
silverfatt <silvestr.fat@mail.ru>
2022-11-04 17:58:38 +0300

    small changes

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
-from database import prepare_db, engine, Document, DB_URL
 from datetime import datetime
+
 from elasticsearch import Elasticsearch
-from fastapi import FastAPI, HTTPException, status
-from helpers import make_query
-from schemas import DocumentScheme
+from fastapi import FastAPI, HTTPException
 from sqlalchemy.orm import sessionmaker
+
+from app.database import prepare_db, engine, Document, DB_URL
+from app.helpers import make_query
+from app.schemas import DocumentScheme
 
 app = FastAPI()
 es = Elasticsearch(hosts=['http://localhost:9200'])
@@ -20,23 +22,27 @@ s = session()
 
 
 @app.get("/")
-async def root():
+async def ping():
     return {"message": "hello"}
 
 
 @app.post("/add")
 async def add_document(document: DocumentScheme):
     """
-    Add new document do database
+    Add a new document do database
     """
     new_document = Document(rubrics=document.rubrics, text=document.text, created_date=datetime.now())
     s.add(new_document)
+    s.commit()
     new_doc_to_es = {
         'text': new_document.text
     }
-    s.commit()
-    resp = es.index(index="doc-index", id=new_document.doc_id, document=new_doc_to_es)
-    print(resp)
+    try:
+        resp = es.index(index="doc-index", id=new_document.doc_id, document=new_doc_to_es)
+    except Exception:
+        s.delete(new_document)
+        s.commit()
+        raise HTTPException(status_code=404, detail="Disconnected from ES")
     return {'id': new_document.doc_id, 'rubrics': new_document.rubrics, 'text': new_document.text,
             'created_date': new_document.created_date}
 
@@ -44,7 +50,7 @@ async def add_document(document: DocumentScheme):
 @app.delete("/delete")
 async def delete_document(doc_id: int):
     """
-    Delete document from database
+    Delete a document from database
     """
     document_to_delete = s.query(Document).filter(Document.doc_id == doc_id).first()
     if not document_to_delete:
@@ -52,7 +58,6 @@ async def delete_document(doc_id: int):
     s.delete(document_to_delete)
     s.commit()
     resp = es.delete(index="doc-index", id=doc_id)
-    print(resp)
     return {'deleted': doc_id}
 
 
